ac9b4db0d09080cb1a2e056e32f0341fbef5f0b8
ninja03jod <149457974+ninja03jod@users.noreply.github.com>
2024-08-26 11:03:03 +0530

    Load pretrained model

---
+++ model.py
@@ -0,0 +1,7 @@
+# model.py
+
+from retinaface import RetinaFace
+
+def load_model():
+    # No need to explicitly load the model in this case; RetinaFace handles it internally.
+    return RetinaFace
